34cd2131378c614b3cfc71db8d622765f4ac9140
JZims <zimmerman.j@hotmail.com>
2026-05-30 04:16:15 -0400

    reset command passing

---
+++ command_reset.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerReset(s *state, cmd command) error {
+	ctx := context.Background()
+
+	if err := s.db.DeleteUser(ctx); err != nil {
+		return err
+	}
+	fmt.Println("Users successfully reset")
+	return nil
+}
